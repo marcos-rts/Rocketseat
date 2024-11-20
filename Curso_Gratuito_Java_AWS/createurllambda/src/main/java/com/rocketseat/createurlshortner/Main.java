b197ab55a7073b11b58fe8b885b21a42092f055e
Marcos Alexandre Reis Torquato dos Santos <marcos.alex1002@gmail.com>
2024-11-19 21:58:35 -0300

    ✨ feat(Main): Criado Lógica para funcionamento da função

--- Curso_Gratuito_Java_AWS/createurllambda/src/main/java/com/rocketseat/createurlshortner/Main.java
+++ Curso_Gratuito_Java_AWS/createurllambda/src/main/java/com/rocketseat/createurlshortner/Main.java
@@ -4,13 +4,31 @@ import java.util.Map;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
-        // TODO Auto-generated method stub
-        // throw new UnsupportedOperationException("Unimplemented method 'handleRequest'");
+        // Recebe o Objeto
+        String body = input.get("body").toString();
+
+        // Variavel do tipo Chave String e Valor String
+        Map<String, String> bodyMap;
+        try {
+            // Vai transformar o Body em um Mapp
+            bodyMap = objectMapper.readValue(body, Map.class);
+
+        } catch (Exception exception) {
+            throw new RuntimeException("Error parsing Json body: " + exception.getMessage(), exception);
+        }
+
+        String originalUrl = bodyMap.get("origunalUrl");
+        String expirationTime = bodyMap.get("expirationMap");
+
         return null;
     }
 }
