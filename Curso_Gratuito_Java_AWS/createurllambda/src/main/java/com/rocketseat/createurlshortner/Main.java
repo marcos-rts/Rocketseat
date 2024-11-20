5dd48d2af63018aeed4fbc74cda395e3b467346c
Marcos Alexandre Reis Torquato dos Santos <marcos.alex1002@gmail.com>
2024-11-19 21:30:46 -0300

    ✨ feat(Main): Adicionado dependencias do AWS

--- Curso_Gratuito_Java_AWS/createurllambda/src/main/java/com/rocketseat/createurlshortner/Main.java
+++ Curso_Gratuito_Java_AWS/createurllambda/src/main/java/com/rocketseat/createurlshortner/Main.java
@@ -1,7 +1,16 @@
 package com.rocketseat.createurlshortner;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Map;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
+
+    @Override
+    public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'handleRequest'");
+        return null;
     }
 }
