3617138bbaa14ae63cb0a353f33d1e12ee9dfae9
Marcos Alexandre Reis Torquato dos Santos <marcos.alex1002@gmail.com>
2024-11-26 01:11:44 -0300

    ✨ feat(UrlData / Main): Adicionado funcionalidade para exportar para JSON

--- Curso_Gratuito_Java_AWS/createurllambda/src/main/java/com/rocketseat/createurlshortner/Main.java
+++ Curso_Gratuito_Java_AWS/createurllambda/src/main/java/com/rocketseat/createurlshortner/Main.java
@@ -16,6 +16,11 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper; 
 // Importa o ObjectMapper, usado para converter objetos Java em JSON e vice-versa.
 
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectAclRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
 public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> { 
     // Define a classe principal que implementa a interface RequestHandler.
     // O tipo genérico `Map<String, Object>` representa o tipo da entrada da Lambda.
@@ -23,6 +28,8 @@ public class Main implements RequestHandler<Map<String, Object>, Map<String, Str
 
     private final ObjectMapper objectMapper = new ObjectMapper(); 
     // Cria uma instância do ObjectMapper para lidar com a conversão entre JSON e objetos Java.
+
+    private final S3Client s3Client = S3Client.builder().build();
 
     @Override 
     public Map<String, String> handleRequest(Map<String, Object> input, Context context) { 
@@ -46,12 +53,30 @@ public class Main implements RequestHandler<Map<String, Object>, Map<String, Str
         // Extrai valores específicos do mapa gerado a partir do JSON.
         String originalUrl = bodyMap.get("origunalUrl"); 
         // Obtém o valor associado à chave "origunalUrl" (nota: há um erro de digitação aqui).
-        String expirationTime = bodyMap.get("expirationMap"); 
-        // Obtém o valor associado à chave "expirationMap".
+        String expirationTime = bodyMap.get("expirationTime"); 
+        // Obtém o valor associado à chave "expirationTime".
+
+        long expirationTimeInSecond = Long.parseLong(expirationTime) * 3600;
 
         // Gera um código curto aleatório usando UUID (Universal Unique Identifier).
         // O código é composto pelos 8 primeiros caracteres do UUID gerado.
         String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
+
+        UrlData urlData =  new UrlData(originalUrl, expirationTimeInSecond);
+
+        try {
+            String urlDataJson = objectMapper.writeValueAsString(urlData);
+
+            PutObjectRequest request = PutObjectRequest.builder()
+            .bucket("mxt-url-shotener-storage")
+            .key(shortUrlCode + ".json")
+            .build();
+
+            s3Client.putObject(request, RequestBody.fromString(urlDataJson));
+
+        } catch (Exception exception) {
+            throw new RuntimeException("Erro saving URL data to S3" + exception.getMessage(), exception); 
+        }
 
         // Cria um mapa para armazenar a resposta que será retornada.
         Map<String, String> response = new HashMap<>();
